adcfe664406266c6770149b93775236a3ddef3d1
Will Dembinski <willdembinski@gmail.com>
2016-09-10 16:13:01 -0700

    First commit

---
+++ src/pages/Detail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Detail extends React.Component {
+    render() {
+        return <p>Tasdasdasdhis is React rendering HTasdasdML!</p>;
+    }
+}
+
+export default Detail;
