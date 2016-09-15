0fc40cee68bcaf923b08b7f8458c1b8945ee10c3
Will Dembinski <willdembinski@gmail.com>
2016-09-14 19:00:27 -0700

    SCOTT PLEASE CODE WITH ME :(

--- src/pages/App.js
+++ src/pages/App.js
@@ -6,15 +6,18 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Unofficial GitHub Browser v0.1</h1>
-                {this.props.children}
+                <div className='container-fluid editor-container clearfix'>
+                    <div className='formBuilderSettings' >
+                        {this.props.children}
+                    </div>
+                <div>
             </div>
-        );
+        )
     }
 }
 
 App.propTypes = {
-    children: React.PropTypes.node,
+    children: React.PropTypes.node
 };
 
 export default App;
